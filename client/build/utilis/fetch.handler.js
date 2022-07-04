e41e16a3ddff50fdd4857adbdded307eadb0438c
Felix Gordon <78864962+fxGRDN@users.noreply.github.com>
2022-07-04 23:16:08 +0200

    Delete client/build directory

--- client/build/utilis/fetch.handler.js
+++
@@ -1,8 +0,0 @@
-export async function getTemplate(module) {
-    let data = await (await fetch(`client\\components\\${module}.html`)).text();
-    let parser = new DOMParser();
-    let componentDoc = parser.parseFromString(data, 'text/html');
-    let component = componentDoc.querySelector('.module-wrapper');
-    let container = component.firstElementChild;
-    return container;
-}
